9e99d0cdc22397e1971635558f234a2c6666943e
arman akther <armanakther@live.co.uk>
2023-02-20 14:18:55 +0000

    Stupid Coach Complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,16 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
+  def answer
+    @question = params[:question]
+
+    if params[:question] == "I am going to work"
+      @coach = 'Great! 🙋‍♂️'
+    elsif params[:question].include? '?'
+      @coach = 'silly question 🤦‍♂️'
+    else
+      @coach = "I don't care, get dressed and go to work! 🙄"
+    end
+  end
 end
